69be9ce187af9b5f285455babf13af315cf1e868
Yunus Emre Yolcu <yemreyolcu@devstudio.design>
2023-01-29 22:25:28 +0300

    Initial commit

---
+++ src/main/java/com/hoaxify/hoaxifyspringboot/HoaxifySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxifyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoaxify/hoaxifyspringboot/HoaxifySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxifyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaxifySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
