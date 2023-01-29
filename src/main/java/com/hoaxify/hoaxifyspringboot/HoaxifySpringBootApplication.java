d712333603ff616bac8c640eda761e7293c1d81b
Yunus Emre Yolcu <yemreyolcu@devstudio.design>
2023-01-29 22:26:04 +0300

    project base created

--- src/main/java/com/hoaxify/hoaxifyspringboot/HoaxifySpringBootApplication.java
+++ src/main/java/com/hoaxify/hoaxifyspringboot/HoaxifySpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HoaxifySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HoaxifySpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HoaxifySpringBootApplication.class, args);
+    }
 
 }
